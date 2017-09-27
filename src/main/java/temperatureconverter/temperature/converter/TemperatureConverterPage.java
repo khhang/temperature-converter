e3507a1f1ae528316e1305b2e83c173f13c60df0
Brian <test@example.com>
2017-09-27 12:35:20 -0700

    updated pom.xml, created classes for tests

---
+++ src/main/java/temperatureconverter/temperature/converter/TemperatureConverterPage.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperatureconverter.temperature.converter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * @author bpham
+ */
+public class TemperatureConverterPage {
+    private WebDriver driver;
+    
+    public TemperatureConverterPage(WebDriver driver){
+        this.driver = driver;
+        System.out.println(driver.getTitle());
+        if(!driver.getTitle().equals("from fahrenheit to celsius - Google Search")){
+            throw new WrongPageException("Incorrect page for Temperature Converter page");
+        }
+    }
+    
+    public void inputFahrenheit(double valueOfFahrenheit){
+        String s = Double.toString(valueOfFahrenheit);
+        driver.findElement(By.xpath(".//*[@id='_Aif']/input")).clear();
+        driver.findElement(By.xpath(".//*[@id='_Aif']/input")).sendKeys(s);
+    }
+    
+    public String actualResult(){
+        String valueOfCelsius = driver.findElement(By.xpath(".//*[@id='_Cif']/input")).getAttribute("value");
+        System.out.println(valueOfCelsius);
+        return valueOfCelsius;
+    }
+    
+    public String expectedResult(){
+        return "37";
+    }
+}
